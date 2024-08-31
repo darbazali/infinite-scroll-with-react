3ab08f26b5dec137825dd2f92e4ecbbe3ffdb6b6
Interceptor <darbaz.me@gmail.com>
2024-08-31 22:15:48 +0300

    Add route for ItemDetailPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import InfiniteScroll from "./InfiniteScroll/InfiniteScroll"
+import ItemDetailPage from "./InfiniteScroll/ItemDetail"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<InfiniteScroll />} />
+        <Route path="/item/:id" element={<ItemDetailPage />} />
       </Routes>
     </BrowserRouter>
   )
